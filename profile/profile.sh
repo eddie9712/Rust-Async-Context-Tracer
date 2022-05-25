98359ac5de0e222a81cc563b36126ed1ac1ce883
eddie9712 <qw1562435@gmail.com>
2022-05-25 21:29:59 +0800

    Fix broken filtering argument

--- profile/profile.sh
+++ profile/profile.sh
@@ -4,7 +4,7 @@ then
 else
         cd ../
         echo "Start reocrding.."
-        uftrace record -F "_<async_std..task..builder..SupportTaskLocals<F> as core..future..future..Future>::poll::_{{closure}}" -N "core::ptr.*" -N "core::alloc.*" -N "core::mem.*" -N "core::sync.*" -N "core::str.*" -N "co    re::iter.*" -N "alloc.*" -N "core::slice.* " -N "core::core_arch.*" -N "core::pin.*" -N "core::cmp.*" $1 
+        uftrace record -F "_<async_std..task..builder..SupportTaskLocals<F> as core..future..future..Future>::poll::_{{closure}}" -N "core::ptr.*" -N "core::alloc.*" -N "core::mem.*" -N "core::sync.*" -N "core::str.*" -N "core::iter.*" -N "alloc.*" -N "core::slice.* " -N "core::core_arch.*" -N "core::pin.*" -N "core::cmp.*" $1 
         uftrace dump > dumped_data.txt
         mv ./dumped_data.txt ./profile/
         cd ./profile
