42b68c3f33a8141af41e2e2c3b893f43782955c5
eddie9712 <qw1562435@gmail.com>
2022-09-10 15:18:13 +0800

    Edit the profile.sh * Let the script can be execute directly * Add support for tokio runtime * Remove the compiler-generated future parser

--- profile/profile.sh
+++ profile/profile.sh
@@ -1,28 +1,38 @@
-if [ -z "$1" ] || [ -z "$2" ]
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
 then
-        echo "The executable name and the output json name should be given"
+    echo "Arguments: [executable file] [output JSON file] [runtime(async_std/tokio)] [get location(optional)]"
 else
         cd ../
         echo "Start reocrding.."
-        uftrace record -P "_<async_std..task..builder..SupportTaskLocals<F> as core..future..future..Future>::poll::_{{closure}}" -P "_<core..future..from_generator..GenFuture<T> as core..future..future..Future>::poll" -P ".*::_\{\{closure\}\}" -P ".*as core..future..future..Future>::poll" $1
+        if [ "$3" = "async_std" ]
+        then
+            uftrace record -P "_<async_std..task..builder..SupportTaskLocals<F> as core..future..future..Future>::poll::_{{closure}}" -P "_<core..future..from_generator..GenFuture<T> as core..future..future..Future>::poll" -P ".*::_\{\{closure\}\}" -P ".*as core..future..future..Future>::poll" $1
+        elif [ "$3" = "tokio" ]
+        then
+            uftrace record -P "tokio..runtime..blocking..task..BlockingTask<T> as core..future..future..Future" -P "tokio::runtime::task::core::CoreStage<T>::poll" -P "_<core..future..from_generator..GenFuture<T> as core..future..future..Future>::poll" -P ".*::_\{\{closure\}\}" -P ".*as core..future..future..Future>::poll" $1
+        fi
         uftrace dump > dumped_data.txt
         mv ./dumped_data.txt ./profile/
         cd ./profile
         echo "Start parsing.."
-        if [ "$3" = "--generated-future" ]
+        if [ "$3" = "async_std" ]
         then
-            echo "(Tracing compiler-generated futures only)" 
-            python3 parser_nu.py $1 $2
-        elif [ "$3" = "--get-location" ]
+            if [ "$4" = "--get-location" ]
+            then
+               python3 parser.py $1 $2 --get-location
+            else
+               python3 parser.py $1 $2
+            fi
+        elif [ "$3" = "tokio" ]
         then
-            echo "(Tracing all futures, get locations for futures)"
-            python3 parser.py $1 $2 --get-location
-        elif ([ "$3" = "--generated-future" ] && [ "$4" = "--get-location" ]) || ([ "$3" = "--get-location" ] && [ "$4" = "--generated-future" ] )
-        then
-            echo "(Tracing compiler-generated futures only, get locations for futures)"
-            python3 parser_nu.py $1 $2 --get-location
+            if [ "$4" = "--get-location" ]
+            then
+               python3 parser_tokio.py $1 $2 --get-location
+            else
+               python3 parser_tokio.py $1 $2
+            fi
         else
-            python3 parser.py $1 $2
+            echo "Wrong arguments."
         fi
         echo "Output the result to the json file!"
         rm ./dumped_data.txt  
